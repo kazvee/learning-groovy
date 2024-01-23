2a41a3309c516891af66288fa914fb5624440850
kazvee <kazveedev@gmail.com>
2024-01-23 13:49:14 -0700

    Add example of conditional structures and loops

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -28,6 +28,21 @@ class HelloWorld {
 
     // Print the full name and age of the Person instance
     println janeSmith.getFullName()
-    println janeSmith.getAge()    
+    println janeSmith.getAge()
+
+        // Identify if Person is middle-aged using a conditional
+    if (janeSmith.getAge() >= 45 && janeSmith.getAge() <= 65) {
+      println janeSmith.getFullName() + " is middle-aged"
+    } else {
+      println janeSmith.getFullName() + " is " + janeSmith.getAge() + " years old"
+    }
+
+    // Define a list of Persons
+    def persons = [janeSmith, new Person(firstName: "Mary", lastName: "Johnson", age: 40)]
+
+    // Iterate over Person instances in list
+    for (Person p : persons) {
+      println p.getFullName()
+    }    
   }
 }
