066d0d3cc03d25a5ac9f3245366d59dd10bdacb7
kazvee <kazveedev@gmail.com>
2024-01-23 13:13:22 -0700

    Add example of variables and data types

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -1,5 +1,23 @@
 class HelloWorld {
   static void main(String[] args) {
     println "Hello World! 🌎"
+
+    // Define a typed variable
+    int age = 20
+
+    // Print variable value
+    println age
+
+    // Print variable type
+    println age.getClass()
+
+    // Define a variable with dynamic typing
+    def name = "Jane"
+
+    // Print variable value
+    println name
+
+    // Print variable type
+    println name.getClass()
   }
 }
