badc40d9f58a7b3bd5832ca085aeb0f658b15f48
kazvee <kazveedev@gmail.com>
2024-01-23 14:01:08 -0700

    Add example of exception handling

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -1,48 +1,57 @@
 class HelloWorld {
-  static void main(String[] args) {
-    println "Hello World! 🌎"
+    static void main(String[] args) {
+        println "Hello World! 🌎"
 
-    // Define a typed variable
-    int age = 20
+        // Define a typed variable
+        int age = 20
 
-    // Print variable value
-    println age
+        // Print variable value
+        println age
 
-    // Print variable type
-    println age.getClass()
+        // Print variable type
+        println age.getClass()
 
-    // Define a variable with dynamic typing
-    def name = "Jane"
+        // Define a variable with dynamic typing
+        def name = "Jane"
 
-    // Print variable value
-    println name
+        // Print variable value
+        println name
 
-    // Print variable type
-    println name.getClass()
+        // Print variable type
+        println name.getClass()
 
-    // Create new Person class and instantiate it
-    Person janeSmith = new Person()
-    janeSmith.setFirstName("Jane")
-    janeSmith.setLastName("Smith")
-    janeSmith.setAge(20)
+        // Create new Person class and instantiate it
+        Person janeSmith = new Person()
+        janeSmith.setFirstName("Jane")
+        janeSmith.setLastName("Smith")
+        janeSmith.setAge(20)
 
-    // Print the full name and age of the Person instance
-    println janeSmith.getFullName()
-    println janeSmith.getAge()
+        // Print the full name and age of the Person instance
+        println janeSmith.getFullName()
+        println janeSmith.getAge()
 
         // Identify if Person is middle-aged using a conditional
-    if (janeSmith.getAge() >= 45 && janeSmith.getAge() <= 65) {
-      println janeSmith.getFullName() + " is middle-aged"
-    } else {
-      println janeSmith.getFullName() + " is " + janeSmith.getAge() + " years old"
+        if (janeSmith.getAge() >= 45 && janeSmith.getAge() <= 65) {
+            println janeSmith.getFullName() + " is middle-aged"
+        } else {
+            println janeSmith.getFullName() + " is " + janeSmith.getAge() + " years old"
+        }
+
+        // Define a list of Persons
+        def persons = [janeSmith, new Person(firstName: "Mary", lastName: "Johnson", age: 40)]
+
+        // Iterate over Person instances in list
+        for (Person p : persons) {
+            println p.getFullName()
+        }
+
+        // Catch unchecked exception and handle it by printing a message
+        try {
+            // Convert a String into a Long data type
+            janeSmith.getFirstName().toLong()
+        } catch (NumberFormatException e) {
+            assert e instanceof NumberFormatException
+            println "☹️ Error: Cannot convert a String into a Long"
+        }
     }
-
-    // Define a list of Persons
-    def persons = [janeSmith, new Person(firstName: "Mary", lastName: "Johnson", age: 40)]
-
-    // Iterate over Person instances in list
-    for (Person p : persons) {
-      println p.getFullName()
-    }    
-  }
 }
