7edb1d77e03974b7b62c6206ab549d0f345df275
kazvee <kazveedev@gmail.com>
2024-01-24 14:44:14 -0700

    Add EqualsAndHashCode and TupleConstructor convenience annotations examples

--- Person.groovy
+++ Person.groovy
@@ -1,54 +1,59 @@
 import groovy.transform.ToString
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.TupleConstructor
 
 // Declare convenience annotations on class-level
 @ToString
+@EqualsAndHashCode
+@TupleConstructor
 
 class Person {
     String firstName
     String lastName
     int age
+}
 
-    Person() {
-    }
+    // Person() {
+    // }
 
-    Person(String firstName, String lastName, int age) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.age = age
-    }
+    // Person(String firstName, String lastName, int age) {
+    //     this.firstName = firstName
+    //     this.lastName = lastName
+    //     this.age = age
+    // }
 
-    @Override
-    boolean equals(o) {
-        if (this.is(o)) {
-            return true
-        }
-        if (!(o instanceof Person)) {
-            return false
-        }
+  // @Override
+  // boolean equals(o) {
+  //     if (this.is(o)) {
+  //         return true
+  //     }
+  //     if (!(o instanceof Person)) {
+  //         return false
+  //     }
 
-        Person person = (Person) o
+  //     Person person = (Person) o
 
-        if (age != person.age) {
-            return false
-        }
-        if (firstName != person.firstName) {
-            return false
-        }
-        if (lastName != person.lastName) {
-            return false
-        }
+  //     if (age != person.age) {
+  //         return false
+  //     }
+  //     if (firstName != person.firstName) {
+  //         return false
+  //     }
+  //     if (lastName != person.lastName) {
+  //         return false
+  //     }
 
-        return true
-    }
+  //     return true
+  // }
 
-    @Override
-    int hashCode() {
-        int result
-        result = (firstName != null ? firstName.hashCode() : 0)
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0)
-        result = 31 * result + age
-        return result
-    }
+    // @Override
+    // int hashCode() {
+    //     int result
+    //     result = (firstName != null ? firstName.hashCode() : 0)
+    //     result = 31 * result + (lastName != null ? lastName.hashCode() : 0)
+    //     result = 31 * result + age
+    //     return result
+    // }
 
     // @Override
     // String toString() {
@@ -58,7 +63,6 @@ class Person {
     //             age +
     //             ")"
     // }
-}
 
 // Previous Learning Notes
 
